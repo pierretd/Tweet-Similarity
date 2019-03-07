9d29c95d3a60e003c26b00dd045ca8da092df78f
Pierre Damiba <pierre.damib@gmail.com>
2019-03-07 12:44:23 -0500

    Deploying

--- twitoff/predict.py
+++ twitoff/predict.py
@@ -14,6 +14,6 @@ def predict_user(user1_name, user2_name, tweet_text, cache=None):
     embeddings = np.vstack([user1_embeddings, user2_embeddings])
     labels = np.concatenate([np.ones(len(user1.tweets)),
                              np.zeros(len(user2.tweets))])
-    log_reg = LogisticRegression().fit(embeddings, labels)
+    log_reg = LogisticRegression(solver='lbfgs').fit(embeddings, labels)
     tweet_embedding = BASILICA.embed_sentence(tweet_text, model='twitter')
     return log_reg.predict(np.array(tweet_embedding).reshape(1, -1))
